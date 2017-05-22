43582bea77fb5994f6f529f98d36b2908e556c09
Elena Morton <elmo@Elenas-MacBook-Pro.local>
2017-05-22 17:44:05 +0100

    Adding POST method to the form.

--- app.rb
+++ app.rb
@@ -11,11 +11,16 @@ end
 
 get '/random-cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample
-  erb(:index)
+  erb :index
 end
 
-get '/named-cat' do
+get '/cat-form' do
+  p params
+  erb :cat_form
+end
+
+post '/named-cat' do
   p params
   @name = params[:name]
-  erb(:index)
+  erb :index
 end
